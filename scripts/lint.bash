cf75c07b580d485f584351bf8cbe483d58f2fb1f
Nanda Lopes <nandalopes@gmail.com>
2023-11-02 19:19:19 -0300

    Generate asdf-racket plugin from template.

--- scripts/lint.bash
+++ scripts/lint.bash
@@ -2,18 +2,9 @@
 
 # lint this repo
 shellcheck --shell=bash --external-sources \
-	setup.bash \
+	bin/* \
+	lib/* \
 	scripts/*
 
 shfmt --language-dialect bash --diff \
-	setup.bash \
-	scripts/*
-
-# lint the template/
-shellcheck --shell=bash --external-sources \
-	template/bin/* --source-path=template/lib/ \
-	template/lib/* \
-	template/scripts/*
-
-shfmt --language-dialect bash --diff \
-	template/**/*
+	./**/*
